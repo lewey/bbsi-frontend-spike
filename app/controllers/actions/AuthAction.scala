1ce77337ed53cf58d633ec30f6b8a79927c0bb33
John Lewis <john.lewis@digital.hmrc.gov.uk>
2018-04-05 10:48:28 +0100

    initiial commit

--- app/controllers/actions/AuthAction.scala
+++ app/controllers/actions/AuthAction.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.actions
 
 import com.google.inject.{ImplementedBy, Inject}
@@ -20,7 +36,7 @@ class AuthActionImpl @Inject()(override val authConnector: AuthConnector, config
   override def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]): Future[Result] = {
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
 
-    authorised().retrieve(Retrievals.externalId) {
+    authorised(ConfidenceLevel.L200 and CredentialStrength(CredentialStrength.strong)).retrieve(Retrievals.externalId) {
       _.map {
         externalId => block(AuthenticatedRequest(request, externalId))
       }.getOrElse(throw new UnauthorizedException("Unable to retrieve external Id"))
